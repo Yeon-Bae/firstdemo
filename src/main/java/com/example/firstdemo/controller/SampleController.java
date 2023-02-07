f610de9288cc61c6d242730b332e9af1ab4d9a48
Yeonbae <kyb4409@gmail.com>
2023-02-07 22:53:24 +0900

    First Commit

---
+++ src/main/java/com/example/firstdemo/controller/SampleController.java
@@ -0,0 +1,36 @@
+package com.example.firstdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SampleController {
+	
+	@GetMapping("/user/userinfo")
+	public String userInfo() {
+		
+		return "/user/userinfo";
+	}
+	
+	@GetMapping("/user/userdata")
+	public String userData(Model model) {
+		
+		model.addAttribute("username", "홍길동");
+		model.addAttribute("username2", "김길동");
+		
+		return "/user/userinfo";
+	}
+	
+	@GetMapping("/user/userid")
+	public String userId(@RequestParam("userid") String userid, Model model ) { 
+		
+		model.addAttribute("userid", userid);
+		model.addAttribute("username", "홍길동");
+		
+		return "/user/userinfo";	// userinfo.html 
+	}
+	
+
+}
