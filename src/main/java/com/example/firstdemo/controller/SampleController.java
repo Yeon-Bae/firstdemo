fca7f9843d4ab5afff8909d7da5bf2442f2f2994
sebin <sebin@SEBINPAPA-home>
2023-02-08 00:07:35 +0900

    주석 추가

--- src/main/java/com/example/firstdemo/controller/SampleController.java
+++ src/main/java/com/example/firstdemo/controller/SampleController.java
@@ -26,6 +26,15 @@ public class SampleController {
 	@GetMapping("/user/userid")
 	public String userId(@RequestParam(value = "userid", required = false) String userid, Model model ) { 
 		
+		/*
+		 * requred=true 옵션은 디폴트가 true이고, 써놓지 않으면 디폴트값으로 true가 적용. 
+		 * 이 경우에는 반드시 userid 파라미터 값을 넣어서 HTTP 요청을 넣어야 한다. 안그러면 페이지 에러가 발생
+		 * http://localhost:8024/user/userid?userid=koreamonster
+		 * 
+		 * 만약, false로 지정을 하면 파라미터 값을 넣지 않고 요청을 해도 페이지 에러가 발생하지 않는다. 
+		 * 
+		 */
+		
 		model.addAttribute("userid", userid);
 		model.addAttribute("username", "홍길동");
 		
